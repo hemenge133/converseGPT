e6f708db309ab400dccad1e4643ade1f8fd7fd15
Hayden Menge <32338288+hemenge133@users.noreply.github.com>
2023-08-17 11:16:44 -0700

    Enable CORS on ALL DOMAINS, fix testing bugs

--- server.py
+++ server.py
@@ -1,7 +1,9 @@
 from flask import Flask, request, jsonify, make_response
 from chatGPT import chat, reset
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/send_message', methods=['POST'])
 def send_message():
