0bb41232bfb9e14c30ebdc07bc37a56e46e7d996
Hayden Menge <32338288+hemenge133@users.noreply.github.com>
2023-08-18 19:20:53 -0700

    test adding back TTS STT

--- chatGPT.py
+++ chatGPT.py
@@ -39,6 +39,22 @@ conversation = LLMChain(
     memory=memory
 )
 
+@contextmanager
+def noalsaerr():
+    ERROR_HANDLER_FUNC = CFUNCTYPE(None, c_char_p, c_int, c_char_p, c_int, c_char_p)
+
+    def py_error_handler(filename, line, function, err, fmt):
+        pass
+
+    c_error_handler = ERROR_HANDLER_FUNC(py_error_handler)
+    asound = cdll.LoadLibrary('libasound.so')
+    asound.snd_lib_error_set_handler(c_error_handler)
+    yield
+    asound.snd_lib_error_set_handler(None)
+
+with noalsaerr():
+    listen = SpeechRecognizer()
+    speak = Speak()
 
 """
 Simple single-user API for now. POST at http:server-ip:5000/send_message
