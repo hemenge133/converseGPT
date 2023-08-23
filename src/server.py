3f0ba9b0f599c390354b6c3a9e2d1e94693f2f76
Hayden Menge <32338288+hemenge133@users.noreply.github.com>
2023-08-22 21:10:40 -0700

    change to port 8000

--- src/server.py
+++ src/server.py
@@ -56,4 +56,4 @@ def reset_chat_agent():
         return make_response("Session reset", 200)
     
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=8000)
