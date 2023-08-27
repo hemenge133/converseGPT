43ea301f6af14782000bf73c9ca7b4b4752cbd0b
Hayden Menge <32338288+hemenge133@users.noreply.github.com>
2023-08-27 03:46:59 -0700

    cleanup

--- initSpeech.py
+++ initSpeech.py
@@ -1,7 +1,8 @@
 from record import SpeechRecognizer
 from speak import Speak
 from contextlib import contextmanager
-from ctypes import *
+from ctypes import CFUNCTYPE, c_char_p, c_int, c_char_p, c_int, c_char_p, cdll
+
 @contextmanager
 def noalsaerr():
     ERROR_HANDLER_FUNC = CFUNCTYPE(None, c_char_p, c_int, c_char_p, c_int, c_char_p)
@@ -14,8 +15,6 @@ def noalsaerr():
     asound.snd_lib_error_set_handler(c_error_handler)
     yield
     asound.snd_lib_error_set_handler(None)
-
-
 
 def initSpeech():
     with noalsaerr():
